46fd2f77e82c95329dfb13c2ddf333510fc91f0a
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2018-04-14 11:03:27 +0200

    login to strava API partly done.

--- PERMON_v2/app.R
+++ PERMON_v2/app.R
@@ -8,43 +8,100 @@
 #
 
 library(shiny)
+library(rStrava)
+library(shinyjs)
+library(httr)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
+source("helpers/configuration.R");
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+Config <<- getConfig("resources/configuration/app_config.json");
+
+APP_URL <- if (interactive()) {
+  options(shiny.port = 8100)
+  "http://localhost:8100/"
+} else {
+  "https://servername/path-to-app"
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+app <- oauth_app(Config$app$name,
+                 key = Config$stravaAPI$clientId,
+                 secret = Config$stravaAPI$secret,
+                 redirect_uri = APP_URL
+)
 
+api <- oauth_endpoint(NULL, "authorize", "token", base_url = "https://www.strava.com/oauth")
+
+scope <- Config$stravaAPI$scope;
+
+has_auth_code <- function(params) {
+  return(!is.null(params$code))
+}
+
+getUserNameFromToken <- function(token) {
+  if(is.null(token)) {
+    return("Not logged");
+  } else {
+    return(token$credentials$athlete$username);
+  }
+}
+
+isTokenSet <- function(session) {
+  if(is.null(session) == FALSE) {
+    if(is.null(session$userData$stoken) == FALSE) {
+      token <- session$userData$stoken;
+      
+      if(is.null(token$credentials) == FALSE) {
+        return(TRUE);
+      }
+    }
+  }
+  
+  return(FALSE);
+}
+
+ui <- fluidPage(
+  verbatimTextOutput("code"),
+  
+  shiny::actionButton(inputId='ab1', label="Loogin", 
+                      icon = icon("th"), 
+                      onclick = sprintf("location.replace('%s', '_blank')", oauth2.0_authorize_url(api, app, scope = scope))),
+  
+  actionButton("logout", "Odhlasit")
+)
+
+uiFunc <- function(req) {
+    ui
+}
+
+server <- function(input, output, session) {
+
+  token <- NULL;
+  params <- parseQueryString(isolate(session$clientData$url_search))
+  
+  if(isTokenSet(session) == FALSE || has_auth_code(params)) {
+    if (!has_auth_code(params)) {
+      return()
+    }
+    
+    # Manually create a token
+    token <- oauth2.0_token(
+      app = app,
+      endpoint = api,
+      credentials = oauth2.0_access_token(api, app, params$code),
+      cache = FALSE
+    )
+    
+    session$userData$stoken <- token;
+    
+    
+  } else {
+    token <- session$userData$stoken;
+  }
+  
+  username = getUserNameFromToken(token);
+  
+  output$code <- renderText(username)
+}
+
+# Note that we're using uiFunc, not ui!
+shinyApp(uiFunc, server)
