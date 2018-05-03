c464fd64a6fde21ce558084df8de2ff4b8150f1e
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2018-05-03 21:21:29 +0200

    Bug with hidden busyindicator solved.

--- PERMON_v2/ui/activitiesDataTable.R
+++ PERMON_v2/ui/activitiesDataTable.R
@@ -1,4 +1,5 @@
 activitiesPanel <- tabPanel("Activities", 
+                               busyIndicator(),
                                DT::dataTableOutput("activitiesTable"),
                                p("Shift+click to select multiple rows"),
                                actionButton("importStream", "Import streams"))
